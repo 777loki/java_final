c6c0adb55733f44ade4b07861b5c087296383862
Enzo Juillard <ejuillard1@myges.fr>
2025-07-17 12:07:02 +0200

    ✅  shouldCreateEmptyHand [TEST]

--- src/main/java/fr/esgi/Hand.java
+++ src/main/java/fr/esgi/Hand.java
@@ -1,4 +1,17 @@
 package fr.esgi;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Hand {
+    private List<String> cards;
+
+    public Hand() {
+        this.cards = new ArrayList<>();
+    }
+
+    public int lenght() {
+        return this.cards.size();
+    }
 }
--- src/test/java/fr/esgi/HandTest.java
+++ src/test/java/fr/esgi/HandTest.java
@@ -1,4 +1,18 @@
 package fr.esgi;
 
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
+
+
 public class HandTest {
+
+    @Test
+    public void shouldCreateEmptyHand() {
+        Hand hand = new Hand();
+        assertEquals(0, hand.lenght());
+    }
+
 }
